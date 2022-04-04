eeeb7ddf50ea8743dcee078ca07d14ad41b94024
NewGraphEnvironment <al@newgraphenvironment.com>
2022-04-04 11:06:42 -0700

    tweak to run all sites with bcfishpass link

--- scripts/02_prep_reporting/0190-build-html-map-tables.R
+++ scripts/02_prep_reporting/0190-build-html-map-tables.R
@@ -1,24 +1,30 @@
+source('scripts/packages.R')
+
 # lets just make the html tables when we need to
 
 
 unlink('docs/sum/bcfp', recursive = T) #erase the file and start over every time
 dir.create('docs/sum/bcfp')
 
-##for now we just print out the phase 2 tables.
-# There are issues with the phase 1s even now we generalized the input column so we can use modelled_crossing_id
-pscis_all %>%
-  # distinct(pscis_crossing_id) %>%
-  filter(source %ilike% 'phase2') %>%
-  pull(pscis_crossing_id) %>%
-  map(fpr_print_tab_bcfp_html)
+
+
+# bcfishpass html to link from map ----------------------------------------
+bcfishpass %>%
+  # need to determine which sites have no bcfishpass info and exclude those so the purrr::map does not bail on us
+  filter(stream_crossing_id %in% (pscis_all %>% pull(pscis_crossing_id))) %>%
+  pull(stream_crossing_id) %>%
+  purrr::map(fpr::fpr_table_bcfp_html)
+
 
 
 unlink('docs/sum/cv', recursive = T) #erase the file and start over every time
 dir.create('docs/sum/cv')
 
-# build all the cv tables for interactive map
+# build all the cv tables for interactive map - be sure to run options(knitr.kable.XXX = '--') in the setup chunk of index.Rmd first
 pscis_all %>%
   distinct(pscis_crossing_id) %>%
   # filter(source %ilike% 'phase2') %>%
   pull(pscis_crossing_id) %>%
-  map(fpr_print_tab_cv_html)
+  map(fpr::fpr_table_cv_html)
+
+
